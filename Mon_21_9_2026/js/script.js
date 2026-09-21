476c878b5d402dc50005ef463f7866ad82a3be76
mohammadH-Ibrahim03 <mohamadhaytham2003@gmail.com>
2026-09-21 15:02:13 +0300

    add comment for the output

--- Mon_21_9_2026/js/script.js
+++ Mon_21_9_2026/js/script.js
@@ -22,31 +22,28 @@ Find the output for these expressions and justify the output according to JavaSc
 ● 1 === '1'
 */
 
+console.log(typeof(100));     // number
+console.log(typeof(73.9));    // number
+console.log(typeof(NaN));     // number
+console.log(typeof("Water")); // string
+console.log(typeof(false));   // boolean
+console.log(typeof(9!=11));   // boolean
 
-console.log(typeof(100));
-console.log(typeof(73.9));
-console.log(typeof(NaN));
-console.log(typeof("Water"));
-console.log(typeof(false));
-console.log(typeof(9!=11));
+console.log("Orang"+"e");      // Orange
+console.log("Orange"+"s");     // Oranges
+console.log("4"+"8");          // 48
+console.log("4"-"8");          // -4
+console.log("name"+3);         // name3
+console.log("name"-3);         // NaN
+console.log(82*"word");        // NaN
+console.log(1+"hello");        // 1hello
+console.log("hello"+1);        // hello1
+console.log(1+true);           // 2
+console.log("hello"+true);     // hellotrue
 
-console.log("Orang"+"e");
-console.log("Orange"+"s");
-console.log("4"+"8");
-console.log("4"-"8");
-console.log("name"+3);
-console.log("name"-3);
-console.log(82*"word");
-console.log(1+"hello");
-console.log("hello"+1);
-console.log(1+true);
-console.log("hello"+true);
-
-console.log(typeof(Infinity));
-console.log(1=='1');
-console.log(1==='1');
-
-
+console.log(typeof(Infinity)); // number
+console.log(1=='1');           // true
+console.log(1==='1');          // false
 
 
 
