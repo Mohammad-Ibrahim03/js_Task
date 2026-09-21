ffec912eeac733dfc9a5cd55164751588d664f25
mohammadH-Ibrahim03 <mohamadhaytham2003@gmail.com>
2026-09-21 14:57:06 +0300

    upload my first task

---
+++ Mon_21_9_2026/js/script.js
@@ -0,0 +1,53 @@
+/*  @everyone 
+Find the output for these expressions and justify the output according to JavaScript interpretation:
+● typeof(100)
+● typeof(73.9)
+● typeof(NaN)
+● typeof("Water")
+● typeof(false)
+● typeof(9 != 11)
+● "Orang" + "e"
+● "Orange" - "s"
+● "4" + "8"
+● "4" - "8"
+● "name" + 3
+● "name" - 3
+● 82 * "word"
+● 1 + "hello"
+● "hello" + 1
+● 1 + true
+● "hello" + true
+● typeof (Infinity)
+● 1 == '1'
+● 1 === '1'
+*/
+
+
+console.log(typeof(100));
+console.log(typeof(73.9));
+console.log(typeof(NaN));
+console.log(typeof("Water"));
+console.log(typeof(false));
+console.log(typeof(9!=11));
+
+console.log("Orang"+"e");
+console.log("Orange"+"s");
+console.log("4"+"8");
+console.log("4"-"8");
+console.log("name"+3);
+console.log("name"-3);
+console.log(82*"word");
+console.log(1+"hello");
+console.log("hello"+1);
+console.log(1+true);
+console.log("hello"+true);
+
+console.log(typeof(Infinity));
+console.log(1=='1');
+console.log(1==='1');
+
+
+
+
+
+
